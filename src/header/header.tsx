2ddf1da62dc2cbc5bc49632f46d3a3f3219211cf
Rayn Yorobe <raynyorobe@gmail.com>
2026-01-13 08:37:43 -0600

    feat: add header component and styles for video game tracker

---
+++ src/header/header.tsx
@@ -0,0 +1,10 @@
+function header() {
+    return (
+        <header>
+            <h1>Video Game Tracker</h1>
+            <span>Keep track of your video game collection!</span>
+        </header>
+    );
+}
+
+export default header;
