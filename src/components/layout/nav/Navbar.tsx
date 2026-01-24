93abcb1677f3c57d9889d7f18253550877d6c7fb
Jared Dube <jdube3@academic.rrc.ca>
2026-01-23 20:37:49 -0600

    refactor: modified to support routing to feature pages

--- src/components/layout/nav/Navbar.tsx
+++ src/components/layout/nav/Navbar.tsx
@@ -1,14 +1,15 @@
+import { Link } from 'react-router-dom';
+
 function Nav() {
     return(
         <nav className="flex justify-between items-center px-4 py-4 bg-neutral-900 border-b border-neutral-800">
             <div className="flex gap-8">
-                <a href="#" className="text-neutral-400 hover:text-neutral-100">Home</a>
-                <a href="#" className="text-neutral-400 hover:text-neutral-100">Video Games</a>
-                <a href="#" className="text-neutral-400 hover:text-neutral-100">About</a>
-                <a href="#" className="text-neutral-400 hover:text-neutral-100">More</a>
+                <Link to="/" className="text-neutral-400 hover:text-neutral-100">Home</Link>
+                <Link to="/browse" className="text-neutral-400 hover:text-neutral-100">Browse Games</Link>
+                <Link to="/profile" className="text-neutral-400 hover:text-neutral-100">Profile</Link>
             </div>
             <div>
-                <a href="#" className="px-4 py-2 bg-neutral-50 text-neutral-950 rounded-md font-medium hover:bg-neutral-200">Log In</a>
+                <Link to="/registration" className="px-4 py-2 bg-neutral-50 text-neutral-950 rounded-md font-medium hover:bg-neutral-200">Sign Up</Link>
             </div>
         </nav>
     );
