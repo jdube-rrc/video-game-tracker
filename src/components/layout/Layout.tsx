e85911ec0dec3a8666b830cf87c0ed7a85199f63
Jared Dube <jdube3@academic.rrc.ca>
2026-01-23 20:39:06 -0600

    refactor: updated to support upcoming feature pages

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,19 +1,16 @@
+import { Outlet } from 'react-router-dom';
 import Nav from "./nav/Navbar";
 import Header from "./header/Header";
 import Footer from "./footer/Footer";
 
-interface LayoutProps {
-    children: React.ReactNode;
-}
-
-function Layout({ children }: LayoutProps) {
+function Layout() {
     return (
         <div className="flex flex-col min-h-screen bg-neutral-950">
             <Nav />
             <div className="max-w-6xl mx-auto px-4 py-8 flex-1 w-full">
                 <Header />
                 <main className="space-y-8">
-                    {children} {/* Render children components here, React will automatically render any nested components passed as children */}
+                    <Outlet /> {/* Renders the current page component based on the route */}
                 </main>
             </div>
             <Footer />
