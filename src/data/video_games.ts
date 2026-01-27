888f1d461b9e1d2fd9ce99dd34df2ebbeb74f8a4
Jared Dube <jdube3@academic.rrc.ca>
2026-01-27 09:09:49 -0600

    experimental: trying a new structuring for mock game data, need to test further

--- src/data/video_games.ts
+++ src/data/video_games.ts
@@ -2,6 +2,14 @@ export interface VideoGame {
   id: number;
   name: string;
   url: string;
+  rating: number;
+  developer: string;
+  publisher: string;
+  initial_release_date: string;
+  platforms: Platforms[];
+  genre: string;
+  multiplayer: boolean;
+  avg_rating: number;
 }
 
 export const videoGames: VideoGame[] = [
@@ -9,76 +17,226 @@ export const videoGames: VideoGame[] = [
     id: 350392,
     name: "Rival Species",
     url: "https://www.igdb.com/games/rival-species",
+    rating: "",
+    artwork_url: "",
+    developer: "",
+    publisher: "",
+    initial_release_date: "",
+    platforms: "",
+    genre: "",
+    multiplayer: "",
+    avg_rating: ""
+
   },
   {
     id: 30492,
     name: "Hearts of Chaos",
     url: "https://www.igdb.com/games/hearts-of-chaos",
+    rating: "",
+    artwork_url: "",
+    developer: "",
+    publisher: "",
+    initial_release_date: "",
+    platforms: "",
+    genre: "",
+    multiplayer: "",
+    avg_rating: ""
+
   },
   {
     id: 339266,
     name: "Power Guy World",
     url: "https://www.igdb.com/games/power-guy-world",
+    rating: "",
+    artwork_url: "",
+    developer: "",
+    publisher: "",
+    initial_release_date: "",
+    platforms: "",
+    genre: "",
+    multiplayer: "",
+    avg_rating: ""
+
   },
   {
     id: 371776,
     name: "Born for the Light: Wavelength Radiant Collapse",
     url: "https://www.igdb.com/games/born-for-the-light-wavelength-radiant-collapse",
+    rating: "",
+    artwork_url: "",
+    developer: "",
+    publisher: "",
+    initial_release_date: "",
+    platforms: "",
+    genre: "",
+    multiplayer: "",
+    avg_rating: ""
+
   },
   {
     id: 376989,
     name: "Dune Buggy",
     url: "https://www.igdb.com/games/dune-buggy",
+    rating: "",
+    artwork_url: "",
+    developer: "",
+    publisher: "",
+    initial_release_date: "",
+    platforms: "",
+    genre: "",
+    multiplayer: "",
+    avg_rating: ""
+
   },
   {
     id: 85727,
     name: "Xuan-Yuan Sword: Mists Beyond the Mountains",
     url: "https://www.igdb.com/games/xuan-yuan-sword-mists-beyond-the-mountains",
+    rating: "",
+    artwork_url: "",
+    developer: "",
+    publisher: "",
+    initial_release_date: "",
+    platforms: "",
+    genre: "",
+    multiplayer: "",
+    avg_rating: ""
+
   },
   {
     id: 63844,
     name: "Ace wo Nerae!",
     url: "https://www.igdb.com/games/ace-wo-nerae",
+    rating: "",
+    artwork_url: "",
+    developer: "",
+    publisher: "",
+    initial_release_date: "",
+    platforms: "",
+    genre: "",
+    multiplayer: "",
+    avg_rating: ""
+
   },
   {
     id: 371149,
     name: "The Deal",
     url: "https://www.igdb.com/games/the-deal--2",
+    rating: "",
+    artwork_url: "",
+    developer: "",
+    publisher: "",
+    initial_release_date: "",
+    platforms: "",
+    genre: "",
+    multiplayer: "",
+    avg_rating: ""
+
   },
   {
     id: 330684,
     name: "Nightmare Kart: The Old Karts",
     url: "https://www.igdb.com/games/nightmare-kart-the-old-karts",
+    rating: "",
+    artwork_url: "",
+    developer: "",
+    publisher: "",
+    initial_release_date: "",
+    platforms: "",
+    genre: "",
+    multiplayer: "",
+    avg_rating: ""
+
   },
   {
     id: 220875,
     name: "My Life: Pet Vet",
     url: "https://www.igdb.com/games/my-life-pet-vet",
+    rating: "",
+    artwork_url: "",
+    developer: "",
+    publisher: "",
+    initial_release_date: "",
+    platforms: "",
+    genre: "",
+    multiplayer: "",
+    avg_rating: ""
+
   },
   {
     id: 94850,
     name: "Safecracker",
     url: "https://www.igdb.com/games/safecracker--1",
+    rating: "",
+    artwork_url: "",
+    developer: "",
+    publisher: "",
+    initial_release_date: "",
+    platforms: "",
+    genre: "",
+    multiplayer: "",
+    avg_rating: ""
+
   },
   {
     id: 152887,
     name: "BanG Dream! Girls Band Party! for Nintendo Switch",
     url: "https://www.igdb.com/games/bang-dream-girls-band-party-for-nintendo-switch",
+    rating: "",
+    artwork_url: "",
+    developer: "",
+    publisher: "",
+    initial_release_date: "",
+    platforms: "",
+    genre: "",
+    multiplayer: "",
+    avg_rating: ""
+
   },
   {
     id: 120563,
     name: "Yokai Dungeon",
     url: "https://www.igdb.com/games/yokai-dungeon",
+    rating: "",
+    artwork_url: "",
+    developer: "",
+    publisher: "",
+    initial_release_date: "",
+    platforms: "",
+    genre: "",
+    multiplayer: "",
+    avg_rating: ""
+
   },
   {
     id: 367989,
     name: "Electro Havoc",
     url: "https://www.igdb.com/games/electro-havoc",
+    rating: "",
+    artwork_url: "",
+    developer: "",
+    publisher: "",
+    initial_release_date: "",
+    platforms: "",
+    genre: "",
+    multiplayer: "",
+    avg_rating: ""
+
   },
   {
     id: 138930,
     name: "Suna no Embrace: Eden no Sato no Never",
     url: "https://www.igdb.com/games/suna-no-embrace-eden-no-sato-no-never",
+    rating: "",
+    artwork_url: "",
+    developer: "",
+    publisher: "",
+    initial_release_date: "",
+    platforms: "",
+    genre: "",
+    multiplayer: "",
+    avg_rating: ""
+
   },
   {
     id: 363578,
@@ -89,20 +247,60 @@ export const videoGames: VideoGame[] = [
     id: 79850,
     name: "Umineko: Ougon Musou Kyoku - Cross",
     url: "https://www.igdb.com/games/umineko-ougon-musou-kyoku-cross",
+    rating: "",
+    artwork_url: "",
+    developer: "",
+    publisher: "",
+    initial_release_date: "",
+    platforms: "",
+    genre: "",
+    multiplayer: "",
+    avg_rating: ""
+
   },
   {
     id: 264844,
     name: "The Fall of Elena Temple",
     url: "https://www.igdb.com/games/the-fall-of-elena-temple",
+    rating: "",
+    artwork_url: "",
+    developer: "",
+    publisher: "",
+    initial_release_date: "",
+    platforms: "",
+    genre: "",
+    multiplayer: "",
+    avg_rating: ""
+
   },
   {
     id: 111651,
     name: "Diablo Immortal",
     url: "https://www.igdb.com/games/diablo-immortal",
+    rating: "",
+    artwork_url: "",
+    developer: "",
+    publisher: "",
+    initial_release_date: "",
+    platforms: "",
+    genre: "",
+    multiplayer: "",
+    avg_rating: ""
+
   },
   {
     id: 274434,
     name: "Merge Games Horror Bundle",
     url: "https://www.igdb.com/games/merge-games-horror-bundle",
+    rating: "",
+    artwork_url: "",
+    developer: "",
+    publisher: "",
+    initial_release_date: "",
+    platforms: "",
+    genre: "",
+    multiplayer: "",
+    avg_rating: ""
+
   },
 ];
