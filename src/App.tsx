929c3ee9cecc420bb3c3e0330efcc73087608b0b
Rayn Yorobe <raynyorobe@gmail.com>
2026-01-27 09:28:55 -0600

    feat: update UserProfile to allow users to edit favorite games

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { useState } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Layout from './components/layout/Layout';
-import HomePage from './components/pages/home-page/HomePage';
-import SearchBrowse from './components/pages/BrowseGames/BrowseGames';
-import GameDetails from './components/pages/game-details/GameDetails';
-import UserProfile from './components/pages/UserProfile/UserProfile';
-import Registration from './components/pages/registration/Registration';
-import { type VideoGame } from './data/video_games';
-import './App.css';
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/layout/Layout";
+import HomePage from "./components/pages/home-page/HomePage";
+import SearchBrowse from "./components/pages/BrowseGames/BrowseGames";
+import GameDetails from "./components/pages/game-details/GameDetails";
+import UserProfile from "./components/pages/UserProfile/UserProfile";
+import Registration from "./components/pages/registration/Registration";
+import { type VideoGame } from "./data/video_games";
+import "./App.css";
 
 function App() {
   const [favorites, setFavorites] = useState<VideoGame[]>([]);
@@ -21,16 +21,35 @@ function App() {
     }
   };
 
-
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<Layout />}>
-          <Route path="/" element={<HomePage visits={visits} setVisits={setVisits} />} />
-          <Route path="/browse" element={<SearchBrowse visits={visits} setVisits={setVisits} />} />
-          <Route path="/game/:id" element={<GameDetails visits={visits} setVisits={setVisits} />} />
-          <Route path="/profile" element={<UserProfile favorites={favorites} onToggleFavorite={toggleFavorite} />} />
-          <Route path="/registration" element={<Registration visits={visits} setVisits={setVisits} />} />
+          <Route
+            path="/"
+            element={<HomePage visits={visits} setVisits={setVisits} />}
+          />
+          <Route
+            path="/browse"
+            element={<SearchBrowse visits={visits} setVisits={setVisits} />}
+          />
+          <Route
+            path="/game/:id"
+            element={<GameDetails visits={visits} setVisits={setVisits} />}
+          />
+          <Route
+            path="/profile"
+            element={
+              <UserProfile
+                favorites={favorites}
+                onToggleFavorite={toggleFavorite}
+              />
+            }
+          />
+          <Route
+            path="/registration"
+            element={<Registration visits={visits} setVisits={setVisits} />}
+          />{" "}
         </Route>
       </Routes>
     </BrowserRouter>
