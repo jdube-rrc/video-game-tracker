3c81295d6ff222e56c711cd8ea8d256dbe5f000f
Rayn Yorobe <raynyorobe@gmail.com>
2026-02-18 14:02:37 -0600

    refactor: remove unused useEffect import and state setter in useGames hook, will be used later on

--- src/hooks/useGames/userGames.ts
+++ src/hooks/useGames/userGames.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { videoGames, type VideoGame } from '../../data/video_games';
 
 /**
@@ -8,7 +8,7 @@ import { videoGames, type VideoGame } from '../../data/video_games';
  * @returns An object containing the list of games and utility functions to access them.
  */
 export function useGames() {
-  const [games, setGames] = useState<VideoGame[]>(videoGames);
+  const [games] = useState<VideoGame[]>(videoGames);
 
   const getGameById = (id: number): VideoGame | undefined => {
     return games.find((game) => game.id === id);
